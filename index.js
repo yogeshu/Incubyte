53a90fbaae9963208b73d8a41751b9c91785c915
Yogeshu <yogeshbhavsar1994@gmail.com>
2024-05-26 22:06:31 +0530

    added final solution for assignment tested with chatGpt solution

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const add = (numbers) => {
   if (numbers === "") {
     return 0;
   }
-  return numbers.split(",").reduce((acc, num) => acc + parseInt(num), 0);
+  //   return numbers.split(",").reduce((acc, num) => acc + parseInt(num), 0);
+
+  const delimiter = numbers.startsWith("//") ? numbers[2] : ",";
+  const nums = numbers.startsWith("//") ? numbers.slice(4) : numbers;
+  const separator = new RegExp(`[${delimiter}\n]`); // copied from chatGpt solution
+  return nums.split(separator).reduce((acc, num) => acc + parseInt(num), 0);
 };
 
 module.exports = { add };
