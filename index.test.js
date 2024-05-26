a71afc8589924cbc25ba151d24d720f14c567252
Yogeshu <yogeshbhavsar1994@gmail.com>
2024-05-26 22:03:25 +0530

    Added New Line Separtor Rule and Custome Delimiter rule

--- index.test.js
+++ index.test.js
@@ -13,3 +13,19 @@ test("should return 1 for a single number", () => {
 test("should return 6 for two numbers", () => {
   expect(add("1,5")).toBe(6);
 });
+
+test("should return 6 for three numbers", () => {
+  expect(add("1,2,3")).toBe(6);
+});
+
+test("should return 10 for 4 numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
+
+test("should return 6 for new line separator", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+test("should return 3 for custom delimiter", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
